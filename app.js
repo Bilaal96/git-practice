2adeac129fe5117f33b13af9bb9bd8dbfc9f528a
Bilaal Ahmad <bilaal-ahmad@hotmail.co.uk>
2022-03-20 16:19:02 +0000

    First commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const alertButton = document.querySelector('.alert-button');
+alertButton.addEventListener('click', function () {
+  alert(
+    '🐱‍👓 Hello Ninja, I am the Git Master! You must focus ☯ it is the only way 🙏'
+  );
+});
